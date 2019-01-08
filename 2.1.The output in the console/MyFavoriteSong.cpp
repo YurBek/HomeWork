6efd08df13d10fc221dbe0384a746b1f762bbe89
YURIY V. ZAKHAROV <YURIY_V_ZAHAROV@MAIL.RU>
2019-01-08 16:32:55 +0300

    2.1.The output in the console

---
+++ 2.1.The output in the console/MyFavoriteSong.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "We Will Rock You\n\r"
+		<< "(Brian May, \"Queen\")\n\n\r"
+
+		<< "Buddy you\'re a boy make a big noise\n\r"
+		<< "Playin\' in the street gonna be a big man some day\n\r"
+		<< "You got mud on yo\' face\n\r"
+		<< "You big disgrace\n\r"
+		<< "Kickin\' your can all over the place\n\n\r"
+		
+		<< "We will we will rock you\n\r"
+		<< "We will we will rock you\n\n\r"
+
+		<< "Buddy you\'re a young man hard man\n\r"
+		<< "Shoutin\' in the street gonna take on the world some day\n\r"
+		<< "You got blood on yo' face\n\r"
+		<< "You big disgrace\n\r"
+		<< "Wavin' your banner all over the place\n\r"
+		
+		<< "We will we will rock you\n\r"
+		<< "We will we will rock you\n\n\r"
+
+		<< "Buddy you\'re an old man poor man\n\r"
+		<< "Pleadin\' with your eyes gonna make you some peace some day\n\r"
+		<< "You got mud on your face\n\r"
+		<< "You big disgrace\n\r"
+		<< "Somebody betta put you back into your place\n\n\r"
+
+		<< "We will we will rock you\n\r"
+		<< "We will we will rock you" << endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
